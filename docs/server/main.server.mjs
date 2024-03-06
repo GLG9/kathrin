4b62184b965a850c70aee400f6dd9212c50d83c0
Gian Gissy <kernel-typisch-03@icloud.com>
2024-03-06 19:16:20 +0100

    builded-home-final

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-TUULYTLT.mjs";import"./chunk-PEM4SF2V.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-7FLHAZO7.mjs";import"./chunk-PEM4SF2V.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
